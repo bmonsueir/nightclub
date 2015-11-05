91f04df0b98525eda8fc1cdd6cef7a03add2073e
Bill Monsueir <bmonsueir@gmail.com>
2015-11-05 12:57:43 -0800

    added user login

--- nightclub.js
+++ nightclub.js
@@ -1,24 +1,56 @@
 if (Meteor.isClient) {
   // counter starts at 0
   //Session.setDefault('counter', 0);
+var bar =[];
+var x = 0;
+var y = 0;
+navigator.geolocation.getCurrentPosition(function(position) {
+location = "Latitude: " +  position.coords.latitude  + "<br> Longitude: " + position.coords.longitude;
+x = position.coords.latitude;
+y = position.coords.longitude;
 
+});
+var clubber = [
+['Bubba', 'El Diablo', 'The Muffin Man', 'Billy the Kid', 'Beevis', 'Butthead'],['Buddy', 'Sparky','Meow'],['Larry', 'Moe','Curly'],['Robin', 'Batgirl','Catwoman'] ];
+var barDep = new Tracker.Dependency();
   Template.night.helpers({
     bars: function () {
-      var bar = [
-        {club: 'The Simpsons',
+
+
+      barDep.depend();
+       bar = [
+        {club: 'Club Pretentious',
         description: 'Yummy goodness',
-        cost: '$$',
         rating: '**',
-        username: ['Bubba will be attending at 10PM'],
-        picture: 'batman.jpg'
+        address: '101 Main Street',
+        city: 'Mayberry',
+        clubber: clubber[0],
+        picture: '1322580608_rdv_kursaal_fahrenheit_bar.jpg'
         },
-        {club: 'The Family Guy',
-        description: 'Pedestrian crap',
-        cost: '$',
+        {club: 'The Snot and Wistle',
+        description: 'British food, crooked teeth, nuff said',
         rating: '*',
-        username: ['Buddy will be attending at 10PM','Sparky will be attending at 11PM', 'Meow will be attending at 12PM'],
-        picture: 'day.jpg'
-        }
+        address: '202 Main Street',
+        city: 'Mayberry',
+        clubber: clubber[1],
+        picture: 'M-Resort-32-Degrees-Draft-Bar-2.jpg'
+      },
+      {club: 'Stooges Stages',
+      description: 'nwuh nwuh nwuh',
+      rating: '**',
+      address: '1150 Market Boulevard',
+      city: 'Mayberry',
+      clubber: clubber[2],
+      picture: 'day.jpg'
+      },
+      {club: 'The Cave',
+      description: 'This club looks like Batman',
+      rating: '****',
+      address: '355 First Avenue',
+      city: 'Mayberry',
+      clubber: clubber[3],
+      picture: 'batman.jpg'
+      }
         ]
 
       return bar;
@@ -26,11 +58,38 @@ if (Meteor.isClient) {
   });
 
   Template.night.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      //Session.set('counter', Session.get('counter') + 1);
+    'click .addClub': function (event) {
+       var index = event.currentTarget.id;
+       var user = Meteor.user();
+
+       if(bar[index].clubber.indexOf(user.username) > -1){
+       //bar[index].clubber.pop();
+       bar[index].clubber.splice(bar[index].clubber.indexOf(user.username), 1);
+     }else{
+
+        bar[index].clubber.push(user.username);
+      }
+        barDep.changed();
+        //update mongo for club's yelp id and twitter names of clubbers and date
+      console.log(bar[index].clubber)
     }
   });
+
+
+  Template.nightclub.events({
+    'click .getLocation': function (event) {
+      if($('[name=location]').val()){
+        console.log($('[name=location]').val());
+      }else{
+      console.log(x,y);
+    }
+      barDep.changed();
+    }
+
+});
+
+
+
 }
 
 if (Meteor.isServer) {
---
+++ user.js
@@ -0,0 +1,46 @@
+if (Meteor.isClient) {
+
+Template.signup.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var email = event.target.email.value;
+        var password = event.target.password.value;
+        var username = event.target.username.value;
+        Accounts.createUser({
+            email: email,
+            username: username,
+            password: password
+        }, function(error){
+            if(error){
+                alert(error.reason);
+            }
+        })
+    }
+});
+
+
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var email = event.target.email.value;
+        var password = event.target.password.value;
+        Meteor.loginWithPassword(email,password, function(error){
+           if(error) {
+            alert(error.reason);
+           }
+
+        });
+
+    }
+});
+
+Template.night.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      console.log('logged out');
+    Meteor.logout();
+    }
+
+});
+
+}
